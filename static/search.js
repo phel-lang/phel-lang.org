f7104f1a48a07359cf0a812ed16d00479890b7ff
Chemaclass <chemaclass@outlook.es>
2023-02-03 23:56:48 +0100

    Scroll up/down in search input

--- static/search.js
+++ static/search.js
@@ -7,6 +7,83 @@ document.addEventListener('keyup', function (keyboardEvent) {
     }
 });
 
+const UP_ARROW = 38;
+const DOWN_ARROW = 40;
+const ENTER_KEY = 13;
+const searchResultsItems = document.getElementById('search-results__items');
+let searchItemSelected;
+let resultsItemsIndex = -1;
+
+document.addEventListener('keydown', function (e) {
+    const len = searchResultsItems.getElementsByTagName('li').length - 1;
+
+    if (e.which === DOWN_ARROW) {
+        downArrow(len);
+    } else if (e.which === UP_ARROW) {
+        upArrow(len);
+    } else if (e.which === ENTER_KEY) {
+        searchItemSelected.getElementsByTagName('a')[0].click();
+    }
+});
+
+function downArrow(len) {
+    resultsItemsIndex++;
+
+    if (!searchItemSelected) {
+        resultsItemsIndex = 0;
+        searchItemSelected = searchResultsItems.getElementsByTagName('li')[0];
+    } else {
+        removeClass(searchItemSelected, 'selected');
+        const next = searchResultsItems.getElementsByTagName('li')[resultsItemsIndex];
+
+        if (typeof next !== undefined && resultsItemsIndex <= len) {
+            searchItemSelected = next;
+        } else {
+            resultsItemsIndex = 0;
+            searchItemSelected = searchResultsItems.getElementsByTagName('li')[0];
+        }
+    }
+
+    searchItemSelected.focus()
+    addClass(searchItemSelected, 'selected');
+}
+
+function upArrow(len) {
+    if (!searchItemSelected) {
+        resultsItemsIndex = -1;
+        searchItemSelected = searchResultsItems.getElementsByTagName('li')[len];
+    } else {
+        removeClass(searchItemSelected, 'selected');
+        resultsItemsIndex--;
+        const next = searchResultsItems.getElementsByTagName('li')[resultsItemsIndex];
+
+        if (typeof next !== undefined && resultsItemsIndex >= 0) {
+            searchItemSelected = next;
+        } else {
+            resultsItemsIndex = len;
+            searchItemSelected = searchResultsItems.getElementsByTagName('li')[len];
+        }
+    }
+    searchItemSelected.focus()
+    addClass(searchItemSelected, 'selected');
+}
+
+function removeClass(el, className) {
+    if (el.classList) {
+        el.classList.remove(className);
+    } else {
+        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+}
+
+function addClass(el, className) {
+    if (el.classList) {
+        el.classList.add(className);
+    } else {
+        el.className += ' ' + className;
+    }
+}
+
 if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
     initSearch();
 } else {
@@ -35,13 +112,23 @@ function initSearch() {
     // Load symbols into elasticlunr object
     window.searchIndexApi.forEach(item => index.addDoc(item));
 
-    $searchInput.addEventListener("keyup", debounce(showResults(index), 150));
+    $searchInput.addEventListener("keyup", function (e) {
+        if (e.which === DOWN_ARROW || e.which === UP_ARROW || e.which === ENTER_KEY) {
+            return;
+        }
+        debounce(showResults(index), 150)();
+    });
+
     // Hide results when user press on the 'x' placed inside the search field
     $searchInput.addEventListener("search", () => $searchResults.style.display = "");
     $searchInput.addEventListener("focusin", function () {
         if ($searchInput.value !== "") {
             showResults(index)();
         }
+    });
+
+    $searchInput.addEventListener("focusout", function () {
+        resultsItemsIndex = -1;
     });
 
     window.addEventListener("click", function (e) {
