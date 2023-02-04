c7ae85f570bdc7f31fb94fce00843145ff5c7b5d
JesusValera <jesus1902@outlook.com>
2023-02-04 18:42:49 +0100

    Refactor search.js file

--- static/search.js
+++ static/search.js
@@ -1,29 +1,33 @@
-////////////////////////////////////
-// Interaction with the search input
-////////////////////////////////////
-document.addEventListener('keyup', function (keyboardEvent) {
-    if (['s', 'S', '/'].includes(keyboardEvent.key)) {
-        document.getElementById("search").focus();
-    }
-});
-
+const MAX_ITEMS = 10;
 const UP_ARROW = "ArrowUp";
 const DOWN_ARROW = "ArrowDown";
 const ENTER_KEY = "Enter";
-const searchResultsItems = document.getElementById('search-results__items');
-let searchItemSelected;
+
+const searchInput = document.getElementById("search");
+const searchResults = document.getElementById("search-results");
+const searchResultsItems = document.getElementById("search-results__items");
+
+let searchItemSelected = null;
 let resultsItemsIndex = -1;
 
-document.addEventListener('keydown', function (keyboardEvent) {
-    const len = searchResultsItems.getElementsByTagName('li').length - 1;
+////////////////////////////////////
+// Interaction with the search input
+////////////////////////////////////
+document.addEventListener("keyup", function (keyboardEvent) {
+    if (["s", "S", "/"].includes(keyboardEvent.key)) {
+        searchInput.focus();
+    }
+});
 
-    console.log(keyboardEvent.key);
+document.addEventListener("keydown", function (keyboardEvent) {
+    const len = searchResultsItems.getElementsByTagName("li").length - 1;
+
     if (keyboardEvent.key === DOWN_ARROW) {
         downArrow(len);
     } else if (keyboardEvent.key === UP_ARROW) {
         upArrow(len);
     } else if (keyboardEvent.key === ENTER_KEY) {
-        searchItemSelected.getElementsByTagName('a')[0].click();
+        searchItemSelected.getElementsByTagName("a")[0].click();
     }
 });
 
@@ -32,48 +36,48 @@ function downArrow(len) {
 
     if (!searchItemSelected) {
         resultsItemsIndex = 0;
-        searchItemSelected = searchResultsItems.getElementsByTagName('li')[0];
+        searchItemSelected = searchResultsItems.getElementsByTagName("li")[0];
     } else {
-        removeClass(searchItemSelected, 'selected');
-        const next = searchResultsItems.getElementsByTagName('li')[resultsItemsIndex];
+        removeClass(searchItemSelected, "selected");
+        const next = searchResultsItems.getElementsByTagName("li")[resultsItemsIndex];
 
         if (typeof next !== undefined && resultsItemsIndex <= len) {
             searchItemSelected = next;
         } else {
             resultsItemsIndex = 0;
-            searchItemSelected = searchResultsItems.getElementsByTagName('li')[0];
+            searchItemSelected = searchResultsItems.getElementsByTagName("li")[0];
         }
     }
 
     searchItemSelected.focus()
-    addClass(searchItemSelected, 'selected');
+    addClass(searchItemSelected, "selected");
 }
 
 function upArrow(len) {
     if (!searchItemSelected) {
         resultsItemsIndex = -1;
-        searchItemSelected = searchResultsItems.getElementsByTagName('li')[len];
+        searchItemSelected = searchResultsItems.getElementsByTagName("li")[len];
     } else {
-        removeClass(searchItemSelected, 'selected');
+        removeClass(searchItemSelected, "selected");
         resultsItemsIndex--;
-        const next = searchResultsItems.getElementsByTagName('li')[resultsItemsIndex];
+        const next = searchResultsItems.getElementsByTagName("li")[resultsItemsIndex];
 
         if (typeof next !== undefined && resultsItemsIndex >= 0) {
             searchItemSelected = next;
         } else {
             resultsItemsIndex = len;
-            searchItemSelected = searchResultsItems.getElementsByTagName('li')[len];
+            searchItemSelected = searchResultsItems.getElementsByTagName("li")[len];
         }
     }
     searchItemSelected.focus()
-    addClass(searchItemSelected, 'selected');
+    addClass(searchItemSelected, "selected");
 }
 
 function removeClass(el, className) {
     if (el.classList) {
         el.classList.remove(className);
     } else {
-        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        el.className = el.className.replace(new RegExp("(^|\\b)" + className.split(" ").join("|") + "(\\b|$)", "gi"), " ");
     }
 }
 
@@ -81,7 +85,7 @@ function addClass(el, className) {
     if (el.classList) {
         el.classList.add(className);
     } else {
-        el.className += ' ' + className;
+        el.className += " " + className;
     }
 }
 
@@ -95,22 +99,19 @@ if (document.readyState === "complete" || (document.readyState !== "loading" && 
 }
 
 function initSearch() {
-    const searchInput = document.getElementById("search");
-    const searchResults = document.querySelector(".search-results");
-
     elasticlunr.trimmer = function (token) {
         if (token === null || token === undefined) {
-            throw new Error('token should not be undefined');
+            throw new Error("token should not be undefined");
         }
 
         return token;
     };
     const index = elasticlunr(function () {
-        this.addField('fnName');
-        this.addField('desc');
-        this.setRef('anchor');
+        this.addField("fnName");
+        this.addField("desc");
+        this.setRef("anchor");
         elasticlunr.stopWordFilter.stopWords = {};
-        elasticlunr.Pipeline.registerFunction(elasticlunr.trimmer, 'trimmer');
+        elasticlunr.Pipeline.registerFunction(elasticlunr.trimmer, "trimmer");
         elasticlunr.tokenizer.seperator = /[\s~~]+/;
     });
     // Load symbols into elasticlunr object
@@ -126,7 +127,7 @@ function initSearch() {
         debounce(showResults(index), 150)();
     });
 
-    // Hide results when user press on the 'x' placed inside the search field
+    // Hide results when user press on the "x" placed inside the search field
     searchInput.addEventListener("search", () => searchResults.style.display = "");
     searchInput.addEventListener("focusin", function () {
         if (searchInput.value !== "") {
@@ -138,9 +139,9 @@ function initSearch() {
         resultsItemsIndex = -1;
     });
 
-    window.addEventListener("click", function (e) {
+    window.addEventListener("click", function (mouseEvent) {
         if (searchResults.style.display === "block") {
-            if (e.target !== searchInput) {
+            if (mouseEvent.target !== searchInput) {
                 searchResults.style.display = "";
             }
         }
@@ -164,11 +165,6 @@ function debounce(func, wait) {
 
 function showResults(index) {
     return function () {
-        const searchInput = document.getElementById("search");
-        const searchResults = document.querySelector(".search-results");
-        const searchResultsItems = document.querySelector(".search-results__items");
-        const MAX_ITEMS = 10;
-
         const term = searchInput.value.trim();
         searchResults.style.display = term === "" ? "" : "block";
         searchResultsItems.innerHTML = "";
@@ -206,18 +202,15 @@ function showResults(index) {
 }
 
 function createMenuItem(result, index) {
-    const searchInput = document.getElementById("search");
-    const searchResultsItems = document.querySelector(".search-results__items");
-
     const item = document.createElement("li");
     item.innerHTML = formatSearchResultItem(result);
-    item.addEventListener('mouseenter', (mouseEvent) => {
+    item.addEventListener("mouseenter", (mouseEvent) => {
         removeSelectedClassFromSearchResult();
-        mouseEvent.target.classList.add('selected');
+        mouseEvent.target.classList.add("selected");
         searchItemSelected = mouseEvent.target;
         resultsItemsIndex = index;
     })
-    item.addEventListener('click', () => searchInput.value = "")
+    item.addEventListener("click", () => searchInput.value = "")
     searchResultsItems.appendChild(item);
 }
 
@@ -230,8 +223,8 @@ function formatSearchResultItem(item) {
 }
 
 function removeSelectedClassFromSearchResult() {
-    const searchResultsItems = document.querySelector(".search-results__items").children;
-    for (let i = 0; i < searchResultsItems.length; i++) {
-        removeClass(searchResultsItems[i], "selected")
+    const searchResultsItemChildren = searchResultsItems.children;
+    for (let i = 0; i < searchResultsItemChildren.length; i++) {
+        removeClass(searchResultsItemChildren[i], "selected")
     }
 }
